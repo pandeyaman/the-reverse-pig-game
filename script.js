9455cff4e2de5be37a8b0daad70fbed117109351
Aman Pandey <pandey.aman111@gmail.com>
2021-07-21 03:12:24 +0530

    feat: added game logic + styling

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ let randomNumber,currentScore;
 
 const rollDice = document.querySelector(".btn-roll-dice");
 const holdScore = document.querySelector(".btn-hold-score");
+const newGame = document.querySelector(".btn-new-game");
 const diceNumber = document.querySelector(".span-dice-number");
 const scorePlayerOne = document.querySelector(".player--1--total--score"); 
 const scorePlayerTwo = document.querySelector(".player--2--total--score"); 
 const currentScorePlayerOne = document.querySelector(".player--1--current--score");
 const currentScorePlayerTwo = document.querySelector(".player--2--current--score");   
+const diceImage = document.querySelector(".img-dice");
 
 let playerOnePlays = true;
 let playerTwoPlays = false;
@@ -45,7 +47,8 @@ function holdAndReduce(){
 
 function addScore(current,total){
     let number = diceNumberGenerator(); 
-    diceNumber.textContent=number;
+    diceImage.src = `img/${number}.png`;
+    diceImage.style.visibility = "visible";
     if(number==6){
         current.textContent="0";
         if(playerOnePlays){
@@ -67,16 +70,39 @@ function holdAndReduceScore(current,total){
     let score = Number(current.textContent); 
     let totalScore = Number(total.textContent);
     totalScore -= score;
-    total.textContent = totalScore;
-    current.textContent = "0";
-    if(playerOnePlays)
-        playerOnePlays = false;
-    else
-        playerOnePlays = true;
+    if(totalScore<=80){
+        total.textContent = "WIN";
+        rollDice.disabled = true;
+        holdScore.disabled = true;
+    }
+    else{
+        total.textContent = totalScore;
+        current.textContent = "0";
+        if(playerOnePlays)
+            playerOnePlays = false;
+        else
+            playerOnePlays = true;
+    }
+}
+
+function resetGame(){
+    scorePlayerOne.textContent = '100';
+    scorePlayerTwo.textContent = '100';
+    currentScorePlayerOne.textContent = '0';
+    currentScorePlayerTwo.textContent = '0';
+    rollDice.disabled=false;
+    holdScore.disabled = false;
+    diceImage.style.visibility = "hidden";
+    isActivePlayer = currentScorePlayerOne;
+    isActiveScore = scorePlayerOne;
+    playerOnePlays=true;
+    playerTwoPlays=false;
+
 }
 
 rollDice.addEventListener('click',playGame);
 holdScore.addEventListener('click',holdAndReduce);
+newGame.addEventListener("click",resetGame);
 
 
 
